cc9b869c9f6ace340c8f98b51d6182b1177c3c0a
Daniel Brewster <dnbrwstr@gmail.com>
2015-02-21 22:09:25 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Attractor: require('./lib/attractor'),
+  Constraints: require('./lib/constraint'),
+  Particle: require('./lib/particle'),
+  Vec2: require('./lib/vec2'),
+  World: require('./lib/world'),
+  util: require('./lib/util')
+};
